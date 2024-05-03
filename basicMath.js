9a19b36da977d5286379cbd1624f928dccd3a95c
Tushar Batra <tusharbatra08@gmail.com>
2024-05-03 16:59:28 +0530

    Counting number of Extracted Digits.

--- basicMath.js
+++ basicMath.js
@@ -4,15 +4,19 @@ let n = 7789;
 // Initialize an array to store the extracted digits
 let extractedDigits = [];
 
+let count = 0;
+
 // Loop until the number is greater than zero
 while (n > 0) {
   let lastDigit = n % 10;
 
   n = Math.floor(n / 10);
 
+  count = count + 1;
+
   extractedDigits.push(lastDigit);
 }
 
 extractedDigits.reverse();
 
-console.log(extractedDigits); 
+console.log('Extracted Digits : ',extractedDigits, 'Count of Extracted Digits is', count); 
