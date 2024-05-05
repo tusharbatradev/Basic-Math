bb06ba3740c6c8208b4f97471ab0c1f3798a9dcf
Tushar Batra <tusharbatra08@gmail.com>
2024-05-05 23:33:26 +0530

    Finding Reverse Number and Checking  Palindrome.

--- basicMath.js
+++ basicMath.js
@@ -3,20 +3,29 @@ let n = 7789;
 
 // Initialize an array to store the extracted digits
 let extractedDigits = [];
-
 let count = 0;
+let reverseNum = 0; 
+let dup = n;
 
 // Loop until the number is greater than zero
 while (n > 0) {
   let lastDigit = n % 10;
-
   n = Math.floor(n / 10);
-
-  count = count + 1;
-
+  count++;
   extractedDigits.push(lastDigit);
+  reverseNum = reverseNum * 10 + lastDigit; 
 }
 
+if(dup === reverseNum){
+  console.log(true)
+} else{
+  console.log(false)
+}
 extractedDigits.reverse();
 
-console.log('Extracted Digits : ',extractedDigits, 'Count of Extracted Digits is', count); 
+console.log(
+  'Extracted Digits : ',extractedDigits,
+  'Count of Extracted Digits is',count,
+  'Reverse Number is',reverseNum
+);
+
